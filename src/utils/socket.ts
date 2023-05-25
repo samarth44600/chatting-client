ebae1d2f31c690c034c375e70d0da53e01cbb319
Samarth Subedi <subedi42samarth@gmail.com>
2023-05-25 16:57:36 +0545

    fixed .env configuration

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const isProduction = process.env.NEXT_APP_NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 const url = isProduction ? "https://chatting-server.up.railway.app/" : "http://localhost:8000/";
 export const socket = io(url, {
     autoConnect: false,
