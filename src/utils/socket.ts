0ea6927eda26dcd999136057bbd8abf2770ecb50
Samarth Subedi <subedi42samarth@gmail.com>
2023-05-25 16:44:12 +0545

    configured socket url

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const url = "http://localhost:8000/";
-
+const isProduction = process.env.NEXT_APP_NODE_ENV === "production";
+const url = isProduction ? "https://chatting-server.up.railway.app/" : "http://localhost:8000/";
 export const socket = io(url, {
     autoConnect: false,
 });
-// socket.on("connect", () => {
-//     console.log("Socket connected", socket.id);
-// });
+socket.on("connect", () => {
+    console.log("Socket connected", socket.id);
+});
